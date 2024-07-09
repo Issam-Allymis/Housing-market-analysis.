8b43a2ad872b583b623f98af1f6e11bbddbe4c7f
Issam <issamally0@gmail.com>
2024-07-09 22:00:37 +0100

    Troubleshooting file errors

--- src/data_management.py
+++ src/data_management.py
@@ -25,7 +25,7 @@ def power_transform(x, power=1.5):
 
 @st.cache_data
 def load_house_prices_data(suppress_st_warning=True, allow_output_mutation=True):
-    print(os.getcwd()) 
+    st.write(os.getcwd()) 
     df = pd.read_csv("inputs/housing-prices-data/house_prices_records.csv") 
     
     return df
