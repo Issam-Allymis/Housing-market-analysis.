30c0f54fa9a96cdede82de24a98473a55651dcf1
Issam <issamally0@gmail.com>
2024-07-09 22:16:09 +0100

    Troubleshooting file errors

--- src/data_management.py
+++ src/data_management.py
@@ -26,7 +26,9 @@ def power_transform(x, power=1.5):
 @st.cache_data
 def load_house_prices_data(suppress_st_warning=True, allow_output_mutation=True):
     st.write(os.getcwd()) 
-    df = pd.read_csv("inputs/housing-prices-data/house_prices_records.csv") 
+    # Construct the file path
+    file_path = os.path.join('inputs', 'housing-prices-data', 'house_prices_records.csv')
+    df = pd.read_csv(file_path) 
     
     return df
 
